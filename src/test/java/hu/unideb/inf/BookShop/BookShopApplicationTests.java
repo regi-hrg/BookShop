96b3ac2e069dc641a9069969e9cfb4ce7bd178b3
regi-hrg <reginahorog4@gmail.com>
2024-12-17 18:49:32 +0200

    Initial commit

---
+++ src/test/java/hu/unideb/inf/BookShop/BookShopApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.BookShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
